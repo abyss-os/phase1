5aa1db4f4d7c4e951fdbd76fef6aee2fec675f6f
Joe Holden <jwh@zorins.us>
2019-11-24 19:37:18 +0000

    drone: install minio-client, check if drone env is set

--- .scripts/drone.sh
+++ .scripts/drone.sh
@@ -5,6 +5,8 @@ echo "trigger: ${DRONE_BUILD_EVENT}"
 echo "previous: ${DRONE_COMMIT_BEFORE}"
 echo "current: ${DRONE_COMMIT_SHA}"
 
+[ ! -z "${MC_HOST_master}" ] && echo "minio, activate!"
+
 OPWD=${PWD}
 
 for PKG in $(git log ...${DRONE_COMMIT_BEFORE} --format=format: --name-only | grep -e 'APKBUILD$' | tac); do
